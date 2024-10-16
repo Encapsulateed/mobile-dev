32a8e7641340f49e41f5079ef39ff8c794b3a7a9
Encapsulateed <alexey.mitroshkin@gmail.com>
2024-10-16 12:39:27 +0300

    lab3 done

---
+++ dart-projects/lab3-server.dart
@@ -0,0 +1,84 @@
+import 'dart:convert';
+import 'dart:io';
+
+const String filePath = 'number.json';
+
+void main() async {
+  final server = await HttpServer.bind(InternetAddress.anyIPv4, 8080);
+
+  print('now listening on http://localhost:8080');
+
+  await for (HttpRequest request in server) {
+    request.response.headers.add('Access-Control-Allow-Origin', '*');
+    request.response.headers
+        .add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    request.response.headers
+        .add('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (request.method == 'OPTIONS') {
+      request.response
+        ..statusCode = HttpStatus.ok
+        ..close();
+      continue;
+    }
+    if (request.method == 'POST' && request.uri.path == '/setNumber') {
+      await handlePost(request);
+    } else if (request.method == 'GET' && request.uri.path == '/getNumber') {
+      await handleGet(request);
+    } else {
+      request.response
+        ..statusCode = HttpStatus.notFound
+        ..write('404 - Not Found')
+        ..close();
+    }
+  }
+}
+
+Future<void> handlePost(HttpRequest request) async {
+  try {
+    String content = await utf8.decoder.bind(request).join();
+    Map<String, dynamic> data = jsonDecode(content);
+    if (data.containsKey('number') && data['number'] is int) {
+      await File(filePath)
+          .writeAsString(jsonEncode({'number': data['number']}));
+
+      request.response
+        ..statusCode = HttpStatus.ok
+        ..write('OK');
+    } else {
+      request.response
+        ..statusCode = HttpStatus.badRequest
+        ..write('Ошибка: поле "number" отсутствует или неверного типа');
+    }
+  } catch (e) {
+    request.response
+      ..statusCode = HttpStatus.internalServerError
+      ..write('Ошибка сервера: $e');
+  } finally {
+    await request.response.close();
+  }
+}
+
+Future<void> handleGet(HttpRequest request) async {
+  try {
+    if (await File(filePath).exists()) {
+      String content = await File(filePath).readAsString();
+      Map<String, dynamic> data = jsonDecode(content);
+      int number = data['number'] ?? 0;
+
+      request.response
+        ..statusCode = HttpStatus.ok
+        ..write(jsonEncode({'number': number}));
+    } else {
+      request.response
+        ..statusCode = HttpStatus.ok
+        ..write(jsonEncode({'number': 0}));
+    }
+  } catch (e) {
+    request.response
+      ..statusCode = HttpStatus.internalServerError
+      ..write('Ошибка сервера: $e');
+  } finally {
+    await request.response.close();
+  }
+}
---
+++ mobile-projects/lab_3/lib/main.dart
@@ -0,0 +1,165 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lab3',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NumberForm(),
+    );
+  }
+}
+
+class NumberForm extends StatefulWidget {
+  @override
+  _NumberFormState createState() => _NumberFormState();
+}
+
+class _NumberFormState extends State<NumberForm> {
+  final URL = 'http://127.0.0.1:8080';
+
+  final _formKey = GlobalKey<FormState>();
+  final _numberController = TextEditingController();
+  int _currentNumber = 0;
+
+  Future<void> sendNumber() async {
+    final url = Uri.parse('${URL}/setNumber');
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'number': _currentNumber,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      print('Num sent: ${_currentNumber}');
+    } else {
+      print('Error: ${response.statusCode}');
+    }
+  }
+  Future<void> sendNumberFromNumberController() async {
+    final url = Uri.parse('${URL}/setNumber');
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'number': int.parse(_numberController.text),
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      print('Num sent: ${_numberController.text}');
+    } else {
+      print('Error: ${response.statusCode}');
+    }
+  }
+//${URL}
+  Future<void> getNumber() async {
+    final url = Uri.parse('${URL}/getNumber');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body);
+
+      setState(() {
+        _currentNumber = jsonResponse['number'] as int;
+      });
+    } else {
+      print('Error: ${response.statusCode}');
+    }
+  }
+
+  void increment() {
+    setState(() {
+      _currentNumber++;
+    });
+
+    sendNumber();
+  }
+
+  void decrement() {
+    setState(() {
+      _currentNumber--;
+    });
+
+    sendNumber();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lab3'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _numberController,
+                decoration: InputDecoration(labelText: 'Запишите число'),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Запишите число';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    sendNumberFromNumberController();
+                  }
+                },
+                child: Text('Отправить'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: getNumber,
+                child: Text('Получить число'),
+              ),
+              const SizedBox(height: 20),
+              Text('Current: $_currentNumber', style: TextStyle(fontSize: 20)),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: decrement,
+                    child: Text('Уменьшить'),
+                  ),
+                  SizedBox(width: 20),
+                  ElevatedButton(
+                    onPressed: increment,
+                    child: Text('Увеличить'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
